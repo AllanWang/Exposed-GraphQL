50839c79921493d01f6d48e5ba153d478029c22a
Allan Wang <me@allanwang.ca>
2018-09-09 17:50:50 -0400

    Fix compile problems

--- exposed-gql-processor/src/main/kotlin/ca/allanwang/exposed/graphql/EntityProcessor.kt
+++ exposed-gql-processor/src/main/kotlin/ca/allanwang/exposed/graphql/EntityProcessor.kt
@@ -1,13 +1,15 @@
 package ca.allanwang.exposed.graphql
 
 import com.squareup.kotlinpoet.FileSpec
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.element.TypeElement
 
 /**
  * Created by Allan Wang on 2018-09-09.
  */
-fun test() {
-    FileSpec.builder("", "")
-
+class EntityProcessor : AbstractProcessor() {
+    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
-
-class T : AbstractProcesso
