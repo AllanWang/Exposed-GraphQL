78243819229f6b8b4597d2571b5bc1deceff1ec7
AllanWang <me@allanwang.ca>
2018-09-09 17:16:14 -0400

    Add initial base

---
+++ exposed-gql-annotation/src/main/kotlin/ca/allanwang/exposed/graphql/Annotations.kt
@@ -0,0 +1,39 @@
+package ca.allanwang.exposed.graphql
+
+/**
+ * Created by Allan Wang on 2018-09-09.
+ */
+
+/**
+ * Used to configure any of the entity's values
+ * For the best performance, properties that are expensive should be implemented through delegation
+ * so that it isn't called unless the value is requested
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+//@Repeatable
+annotation class GraphQLField(
+        /**
+         * Field name
+         * Defaults to the property name
+         */
+        val name: String = "",
+
+        /**
+         * Tag identifier to allow for different graphql entities based around the same exposed entity
+         */
+        val tag: String = "",
+
+        /**
+         * Optional description
+         */
+        val description: String = "")
+
+/**
+ * Skips this field when generating graphql object entry
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class GraphQLFieldIgnore
---
+++ exposed-gql-processor/src/main/kotlin/ca/allanwang/exposed/graphql/EntityProcessor.kt
@@ -0,0 +1,13 @@
+package ca.allanwang.exposed.graphql
+
+import com.squareup.kotlinpoet.FileSpec
+
+/**
+ * Created by Allan Wang on 2018-09-09.
+ */
+fun test() {
+    FileSpec.builder("", "")
+
+}
+
+class T : AbstractProcesso
